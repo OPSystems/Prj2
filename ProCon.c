d747fdd9de6e483c4d9c1d9f87b33f31c2d90ad5
tellis6 <tellis6@calstatela.edu>
2020-10-29 14:29:40 -0700

    adding all three questions with readme

--- ProCon.c
+++ ProCon.c
@@ -2,51 +2,70 @@
 #include <semaphore.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
+// global variables
 #define MAX_SIZE 5 
 pthread_mutex_t bufLock;
 sem_t empty, full;
+int value;
 
+/*producer function, 
+to produce a printable character into the buffer 
+if there is available space*/
 void *producer(char* buf)
 {   
+    // this will be used to represent index
     int in = 0;
-    for(int i = 0; i < MAX_SIZE; i++) {
-        int item = rand()%(10-0) + 0;
-        sem_wait(&full);
-        pthread_mutex_lock(&bufLock);
-        buf[in] = item;
-        printf("Producer inserted %d at index %d\n", buf[in],in);
-        in = (in+1) % MAX_SIZE;
-        pthread_mutex_unlock(&bufLock);
-        sem_post(&empty);
+
+    // infinite loop to continue producing until user exits
+    for(;;) {
+        sem_wait(&full);                                            // wait till there is available space
+        pthread_mutex_lock(&bufLock);                               // aquire lock to access critical section
+        char pItem = 'a' + (rand() % 26);                           // generate a random character to place in buffer
+        buf[in] = pItem;                                            // place the character in the buffer
+        printf("Producer inserted %c at index %d\n", buf[in],in);   // print a message showing the new character and its buffer location
+        in = (in+1) % MAX_SIZE;                                     // increment index till MAX_SIZE, then reset to 0
+        pthread_mutex_unlock(&bufLock);                             // release lock
+        sem_post(&empty);                                           // signal the consumer
     }
 }
 
 void *consumer(char* buf)
 {   
+    // this will be used to represent index
     int out = 0;
-    for(int i = 0; i < MAX_SIZE; i++) {
-        sem_wait(&empty);
-        pthread_mutex_lock(&bufLock);
-        int item = buf[out];
-        printf("Consumer removed %d from index %d\n",item, out);
-        out = (out+1) % MAX_SIZE;
-        pthread_mutex_unlock(&bufLock);
-        sem_post(&full);
+
+    // infinite loop to continue consuming until user exits
+    for(;;) {
+        sem_wait(&empty);                                           // wait till there is a character to consume
+        pthread_mutex_lock(&bufLock);                               // aquire lock to access critical section
+        char cItem = buf[out];                                      // variable to hold the character from the 'out' position in buffer
+        printf("Consumer removed %c from index %d\n",cItem, out);   // print a message showing the character to be removed and its buffer location
+        out = (out+1) % MAX_SIZE;                                   // increment index till MAX_SIZE, then reset to 0
+        pthread_mutex_unlock(&bufLock);                             // release lock
+        sem_post(&full);                                            // signal the producer
     }
 }
 
 int main()
 {   
-    srand(time(NULL));
-    int buffer[MAX_SIZE];
-    pthread_t p;
-    pthread_mutex_init(&bufLock, NULL);
-    sem_init(&full,0,MAX_SIZE);
-    sem_init(&empty,0,0);
+    srand(time(NULL));                                      // used to generate random char
+    char buffer[MAX_SIZE];                                  // create the buffer with size MAX_SIZE
+    pthread_t p, c;                                         // declare threads
+    pthread_mutex_init(&bufLock, NULL);                     // initialize mutex 
+    sem_init(&full,0,MAX_SIZE);                             // initialize 'full' semaphore, set to MAX_SIZE 
+    sem_init(&empty,0,0);                                   // initialize 'empty' semaphore, set to 0
 
-    pthread_create(&p, NULL, (void *)producer, &buffer);
-    consumer(&buffer);
+    pthread_create(&p, NULL, (void *)producer, &buffer);    // create producer thread and call the producer function, passing buffer
+    pthread_create(&c, NULL, (void *)consumer, &buffer);    // create producer thread and call the consumer function, passing buffer
+    
+    pthread_join(p, NULL);                                  // join the producer thread
+    pthread_join(c, NULL);                                  // join the consumer thread
+
+    pthread_mutex_destroy(&bufLock);                        // destroy mutex
+    sem_destroy(&full);                                     // destroy 'full' semaphore
+    sem_destroy(&empty);                                    // destroy 'empty' semaphore
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,8 +1,31 @@
-# Prj2
+# Prj2: Exercises in Concurrency and Synchronization
 
-Exercises in Concurrency and Synchronization
+## - Harshil Kotamreddy, Tim Ellis, Lynne Tien
 
-Objectives:
-Learn to:
-• Solve inter-process communication problems during concurrent execution of processes.
-• Use Posix Pthread library for concurrency.
+**Objectives**
+<p>Learn to:<br>
+- Solve inter-process communication problems during concurrent execution of processes.<br>
+- Use Posix Pthread library for concurrency.</p>
+<br>
+<p> Instructions To Run Each Program:<br>
+
+1. Bounded-buffer Producer/Consumer problem:<br>
+    - Open terminal
+    - <code>cd</code> into the directory where the file resides
+    - At the command prompt, type <code>gcc procon.c -o procon -lpthread</code> then press enter
+    - Then type <code>./procon</code> and again press enter
+    <br>
+    
+2. Mother Hubbard (children, mother, father)
+    - Open terminal
+    - <code>cd</code> into the directory where the file resides
+    - At the command prompt, type <code>gcc mh.c -o mh -lpthread</code> then press enter
+    - Then type: <code>./mh `<number of days>`</code> and again press enter. For example typing <code>./mh 2</code> will launch the program for 2 days.
+    <br>
+    
+3. Airline Passengers
+    - Open terminal
+    - <code>cd</code> into the directory where the file resides
+    - At the command prompt, type <code>gcc airline.c -o airline -lpthread</code> then press enter
+    - Then type <code>./airline `<number of passengers>` `<number of baggage handlers>` `<number of security screeners>` `<number of flight attendants>`</code> and again press enter. For example typing <code>./airline 100 3 5 2</code> will launch the program with 100 passengers, 3 baggage handlers, 5 security screeners, 2 flight attendants.
+    <br>
